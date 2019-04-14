818a6a6a9df319de5fc5238dceff73457089ae44
Roberto <rsstocast@gmail.com>
2019-04-13 21:59:16 -0600

    Cambios

---
+++ Lab4/app/src/main/java/com/example/lab4/Lugar2.java
@@ -0,0 +1,55 @@
+package com.example.lab4;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class Lugar2 extends Fragment {
+
+    EditText et;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view =inflater.inflate(R.layout.fragment_lugar2, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button boton2=(Button)view.findViewById(R.id.boton2);
+        et = (EditText)view.findViewById(R.id.editText2);
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String a = et.getText().toString();
+                et.setText(a);
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, a );
+                startActivity(intent.createChooser(intent, "Compartir Comentario"));
+
+            }
+        });
+    }
+
+
+
+}
